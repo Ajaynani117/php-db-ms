cf9433f664cfdb50584ae1a1b03be2f8bd16e8df
Ajaynani117 <sriramajay1173@gmail.com>
2025-04-15 19:37:01 -0500

    docker-composr install steps

--- deployConfig/docker-script.sh
+++ deployConfig/docker-script.sh
@@ -2,3 +2,5 @@
 sudo yum install docker -y
 sudo systemctl enable docker
 sudo systemctl start docker
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
